0934820424b29fc6c9d829cab7068afcfd1f3fa3
RagasriGalla <ragasrigalla@gmail.com>
2026-04-05 15:18:10 +0530

    Create selection_sort.py

---
+++ sorting_techniques/selection_sort.py
@@ -0,0 +1,9 @@
+class Solution: 
+    def selectionSort(self, arr):
+        for i in range(len(arr)):
+            min_ind=i
+            for j in range(i+1,len(arr)):
+                if(arr[j]<arr[min_ind]):
+                    min_ind=j
+            arr[i],arr[min_ind]=arr[min_ind],arr[i]
+        return arr

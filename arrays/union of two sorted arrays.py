0e7e886c06e8800b1fbd670a34e32ce9835b6480
RagasriGalla <ragasrigalla@gmail.com>
2026-04-07 18:16:13 +0530

    Create union of two sorted arrays.py

---
+++ arrays/union of two sorted arrays.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findUnion(self, a, b):
+        i=0
+        j=0
+        res=[]
+        while i<len(a) and j<len(b):
+            if(a[i]<b[j]):
+                if(len(res)==0 or res[-1]!=a[i]):
+                    res.append(a[i])
+                i+=1
+            elif(b[j]<a[i]):
+                if(len(res)==0 or res[-1]!=b[j]):
+                    res.append(b[j])
+                j+=1
+            else:
+                if(len(res)==0 or res[-1]!=b[j]):
+                    res.append(b[j])
+                i+=1
+                j+=1
+        while i<len(a):
+            if(len(res)==0 or res[-1]!=a[i]):
+                res.append(a[i])
+            i+=1
+        while j<len(b):
+            if(len(res)==0 or res[-1]!=b[j]):
+                res.append(b[j])
+            j+=1
+        return res
